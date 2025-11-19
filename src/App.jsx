a91a1b427e8b13c53b38a8cc0fd83695befc76f7
Miskron <miskronaljahidin8@gmail.com>
2025-11-19 22:04:05 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+
+function App() {
+  return (
+
+    <div className="bg-black text-white p-10">
+      <Card>uhuy </Card>
+    </div>
+  )
+}
+
+export default App
